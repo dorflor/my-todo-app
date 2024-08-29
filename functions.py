3ee56b006d025d9a569c1af7870535c7fc99ab7f
albasd01 <Doru.Albastroiu@imperialdade.com>
2024-08-29 10:30:16 -0400

    Initial commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILE_PATH = "todos.txt"
+
+
+def get_todos(filepath=FILE_PATH):
+    """ Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILE_PATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello from functions!")
+    print(get_todos())
